b1b8f3e7836433c9b1fca785c5f2ac9836d49238
Lee Jeongmin <102647148+min1018@users.noreply.github.com>
2024-04-08 22:13:46 +0900

    Title: 왕실의 기사 대결, Time: 212ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240408/왕실의 기사 대결/royal-knight-duel.py
+++ 240408/왕실의 기사 대결/royal-knight-duel.py
@@ -4,20 +4,21 @@ import time
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-flag = 0
 
 def moveKnight(num, dir):
-    global flag
-    if power[num-1] - damage[num-1] == 0:
-        flag = -1
-        return -1
+
+    if power[num-1] - damage[num-1] <= 0:
+        return
     q = deque()
-    moved = [[] for _ in range(n)]
+    moved = [[] for _ in range(mem)]
+    #print("start knight", knight)
+    #print("start move", moved)
+
     q.append(num-1)
     while q:
         pushed = q.popleft()
         #print("pushed", pushed)
-        if power[pushed] - damage[pushed] == 0: # 이미 죽은 기사
+        if power[pushed] - damage[pushed] <= 0: # 이미 죽은 기사
             continue
         for k in knight[pushed]: # 산 기사
             #time.sleep(1)
@@ -27,50 +28,32 @@ def moveKnight(num, dir):
                 # 빈칸 혹은 기사 있음
                 if graph[nx][ny] != 2: # 벽 아님
                     moved[pushed].append([nx, ny])
-                    for i in range(n): # 빈칸
+                    for i in range(mem):
                         #print(nx, ny, "knight[i]", knight[i])
-                        if i == pushed or [nx, ny] not in knight[i]:
+                        if i == pushed or [nx, ny] not in knight[i]: # 빈칸
                             continue
                         else: # 기사 있음
                             q.append(i)
                             #print("append", i)
-                else:
+                else: # 벽이라서 못가
                      return -1
             else: # 범위 밖으로 가는 애가 하나라도 있음
                 return -1
-    for t in range(n):
+    for t in range(mem):
         if moved[t]:
             knight[t] = moved[t]
+    #print("knight", knight)
     #print("moved", moved)
-    for i in range(n):
+    for i in range(mem):
         for k in range(len(moved[i])):
             #print("i", i) # i가 1인경우
             if i != num - 1:  # 명령 받은 애는 데미지 안 받음
                 x, y = moved[i][k][0], moved[i][k][1]
                 if graph[x][y] == 1:  # 덫이 있고
-                    #print(x, y)
-                    if damage[i] > power[i]:
-                        #print("in if")
-                        continue
-                    else:
-                        #print("in else")
-                        damage[i] += 1
+                    damage[i] += 1
 
     # 명령을 받은 기사는 데미지 없음
     # 밀쳐진 기사는 도착지에 함정 잇으면 데미지
-
-
-# def calcDamage(num, moved):
-#     for i in range(n):
-#         for k in range(len(moved[i])):
-#             if i != num-1: # 명령 받은 애는 데미지 안 받음
-#                 x, y = moved[i][k][0], moved[i][k][1]
-#                 if graph[x][y] == 1: # 덫이 있고
-#                     if damage[i] > power[i]:
-#                         continue
-#                     else:
-#                         damage[i] += 1
-
 
 
 
@@ -79,46 +62,30 @@ graph = [list(map(int, input().split(" "))) for _ in range(n)]
 visited = [[0] * n for _ in range(n)]
 
 
-knight = [[] for _ in range(n)]
-power = [0 for _ in range(n)]
-damage = [0 for _ in range(n)]
-for i in range(1, mem+1):
+knight = [[] for _ in range(mem)]
+power = [0 for _ in range(mem)]
+damage = [0 for _ in range(mem)]
+for i in range(mem):
     x, y, h, w, strength = map(int, input().split(" "))
     x, y = x-1, y-1
-    power[i-1] = strength
+    power[i] = strength
     #print(x,y,h,w)
     for q in range(x, x+h):
         for h in range(y, y+w):
             #print(q, h)
-            knight[i-1].append([q, h])
-
-
+            knight[i].append([q, h])
 
 for i in range(1,order+1):
     #print("try", i)
     num, dir = map(int, input().split(" "))
     #print("original", knight)
-    t = moveKnight(num, dir)
-    #print("after",knight)
+    moveKnight(num, dir)
 
+    #print("after",knight)
     #print("damage", damage)
-    # if t == -1:
-    #     continue
-    # else:
-    #     calcDamage()
 
 ans = 0
-for i in range(n):
-    if power[i] - damage[i] != 0:
+for i in range(mem):
+    if power[i] - damage[i] > 0:
         ans += damage[i]
 print(ans)
-
-# def gravity():
-#     n = len(arr)
-#     m = len(arr[0])
-#     for i in range(n-1):
-#         for k in range(m):
-#             p = i
-#             while 0 <= p and arr[p][j] == 1 and arr[p+1][j] == 0:
-#                 arr[p][j], arr[p+1][j] = arr[p+1][j], arr[p][j]
-#                 p -= 1
