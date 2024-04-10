e07313e47855bb788333fd1a2e91f648dca80164
Lee Jeongmin <102647148+min1018@users.noreply.github.com>
2024-04-10 13:17:08 +0900

    Title: 싸움땅, Time: 145ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240410/싸움땅/battle-ground.py
+++ 240410/싸움땅/battle-ground.py
@@ -29,31 +29,28 @@ def play(px, py, d, power, gun, i):
         if num != i and px == player[num][0] and py == player[num][1]: 
             flag = 1
             p1 = power + gun
-            p2 = player[num][3]+player[num][4]
-            win = 0
-            # 포인트 획득 및 진사람 땅에 총 놓기
+            p2 = player[num][3] + player[num][4]
+            win, loose = -1, -1
+            # 승부
             if p1 > p2 : # i가 이김 
-                point[i] += abs(p1- p2)
                 win, loose = i, num
-                graph[px][py].append(player[num][4])
             elif p1 < p2: # num 이 이김 
-                point[num] += abs(p1- p2)
                 win, loose = num, i
-                graph[px][py].append(player[i][4])
             elif p1 == p2: 
                 if power > player[num][3]:
-                    point[i] += abs(p1- p2)
                     win, loose = i, num
-                    graph[px][py].append(player[num][4])
                 else:
-                    point[num] += abs(p1- p2)
                     win, loose = num, i
-                    graph[px][py].append(player[i][4])
+            #포인트 획득
+            point[win] += abs((player[loose][3]+player[loose][4]) - (player[win][3]+player[win][4]))
+            # 진사람 총 두고 감 
+            graph[px][py].append(player[loose][4])
+            player[loose][4] = 0 
 
             # 이긴 사람 총 교체 
             for t in range(len(graph[px][py])):
                 if player[win][4] < graph[px][py][t]:
-                    player[win][4], graph[px][py][t] = player[win][4], graph[px][py][t]
+                    player[win][4], graph[px][py][t] = graph[px][py][t], player[win][4]
 
             # player 진 -> 간 땅에 초이 있으면 줍, 없으면 gun 0
             while True:
@@ -73,26 +70,25 @@ def play(px, py, d, power, gun, i):
                     
                 # 사람 있으면 혹은 벽이라면 -> 90도 회전 
                 if sign == 0:
-                    player[loose][0], player[loose][1] = nx, ny
-                    if len(graph[nx][ny]) > 0:
-                        for tg in range(len(graph[nx][ny])):
-                            if graph[nx][ny][tg] > 0:
-                                player[loose][4], graph[nx][ny][tg] = graph[nx][ny][tg], player[loose][4]
                     break
+                
+            player[loose][0], player[loose][1] = nx, ny
+            for tg in range(len(graph[nx][ny])):
+                if graph[nx][ny][tg] > 0:
+                    player[loose][4], graph[nx][ny][tg] = graph[nx][ny][tg], player[loose][4]
 
+            
 
-    if flag == 0: 
+    if flag == 0: # 이동한 방향에 플레이어가 없다면 
         if len(graph[px][py]) > 0: # 2. 총 있음 
             for g in range(len(graph[px][py])):
-                if graph[px][py][g] > gun :# 더 큰 총이 있으면 
+                if graph[px][py][g] > player[i][4] :# 더 큰 총이 있으면 
                     graph[px][py][g], player[i][4] = player[i][4], graph[px][py][g] # 바꿔치기
 
-        else: # 그냥 빈칸 
+        else: # 3. 그냥 빈칸 
             player[i] = [px, py, d, power, gun]
 
     
-
-
 
 
 n, mem, game = map(int, input().split())
