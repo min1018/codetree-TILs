55a119781002c164d6ca0a64fbb55dc780b5117d
Lee Jeongmin <102647148+min1018@users.noreply.github.com>
2024-04-11 21:38:14 +0900

    Title: 나무 타이쿤, Time: 179ms, Memory: 25MB, Status: Passed - CodeTree

--- 240411/나무 타이쿤/tree-tycoon.py
+++ 240411/나무 타이쿤/tree-tycoon.py
@@ -1,61 +1,66 @@
 dx = [0, -1, -1, -1, 0, 1, 1, 1]
 dy = [1, 1, 0, -1, -1, -1, 0, 1]
 
-def medMove(med, d, far):
-    newMed = [[0] * n for _ in range(n)]
+def medMove(initmed, d, far): # 특수 영양제 이동
+    currMed = [[0] * n for _ in range(n)]
     for i in range(n):
         for k in range(n):
-            if med[i][k] == 1:
+            if initmed[i][k] == 1:
                 nx = (i + dx[d-1]*far) % n
                 ny = (k + dy[d-1]*far) % n
                 while nx < 0:
                     nx += n
                 while ny < 0:
                     ny += n
-                if 0 <= nx < n and 0 <= ny < n:
-                    newMed[nx][ny] = 1
-    return newMed
+                currMed[nx][ny] = 1
+    return currMed
 
-def grow(med):
+def grow(currmed):
+    medWas = []
     tx = [-1, 1, -1, 1]
     ty = [1, -1, -1, 1]
     for i in range(n):
         for k in range(n):
-            if med[i][k] == 1:
+            if currmed[i][k] == 1:
+                medWas.append((i,k))
                 graph[i][k] += 1
-    for a in range(n):
-        for b in range(n):
-            if med[a][b] == 1:
-                for h in range(4):
-                    nx = a + tx[h]
-                    ny = b + ty[h]
-                    if 0 <= nx < n and 0 <= ny < n:
-                        if graph[nx][ny] >= 1:
-                            graph[a][b] += 1
+    #print("drink med", graph)
+    for x, y in medWas:
+        for h in range(4):
+            nx = x + tx[h]
+            ny = y + ty[h]
+            if 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] >= 1:
+                    graph[x][y] += 1               
+    return medWas
 
-def makeMed(med):
+def makeMed(medWas):
+    nextMed = [[0] * n for _ in range(n)]
     for i in range(n):
         for k in range(n):
-            if graph[i][k] >= 2 and med[i][k] != 1:
+            if (i, k) not in medWas and graph[i][k] >= 2:
                 graph[i][k] -= 2
-                med[i][k] = 1
-            elif med[i][k] == 1:
-                med[i][k] = 0
-    return med
+                nextMed[i][k] = 1
+    return nextMed
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-med = [[0] * n for _ in range(n)]
-med[n-1][0] = 1
-med[n-1][1] = 1
-med[n-2][0] = 1
-med[n-2][1] = 1
-d, far = map(int, input().split())
+initmed = [[0] * n for _ in range(n)]
+# 초기 영양제 
+initmed[n-1][0] = 1
+initmed[n-1][1] = 1
+initmed[n-2][0] = 1
+initmed[n-2][1] = 1
 
 for _ in range(m):
-    currmed = medMove(med, d, far)
-    grow(currmed)
-    med = makeMed(currmed)
+    d, far = map(int, input().split())
+    currmed = medMove(initmed, d, far)
+    #print("after med move", currmed)
+    medWas = grow(currmed)
+    #print("after grow", graph)
+    initmed = makeMed(medWas)
+    #print("all done", graph)
+    #print("next med", med)
 
 ans = 0
 for i in range(n):
